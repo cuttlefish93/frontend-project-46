2afea448574e6c072f2ab27d87dc48e56799ee89
Kamila Begimkulova <cuttlefish93@gmail.com>
2023-06-16 18:05:48 +0500

    add basic information for cli

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number');
+
+program
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
