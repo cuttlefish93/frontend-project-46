06e8fe7eff04ce2b00afc46c869bc3c4b80995ad
Kamila Begimkulova <cuttlefish93@gmail.com>
2023-06-16 20:28:19 +0500

    add genDiff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import { genDiff } from '../index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program
   .helpOption('-h, --help', 'output usage information');
---
+++ index.js
@@ -0,0 +1,64 @@
+import * as path from 'node:path';
+import * as process from 'process';
+import * as fs from 'fs';
+import _ from 'lodash';
+
+function parseJson(json) {
+  return JSON.parse(json);
+}
+
+const formats = {
+  json(content) {
+    return parseJson(content);
+  },
+};
+
+function genDiff(filepath1, filepath2) {
+  let result = '';
+
+  const fullPath1 = path.resolve(process.cwd(), filepath1);
+  const fullPath2 = path.resolve(process.cwd(), filepath2);
+
+  const content1 = fs.readFileSync(fullPath1, { encoding: 'utf8' });
+  const content2 = fs.readFileSync(fullPath2, { encoding: 'utf8' });
+
+  const ext1 = filepath1.split('.').at(-1);
+  const ext2 = filepath2.split('.').at(-1);
+
+  const parsedContent1 = formats[ext1](content1);
+  const parsedContent2 = formats[ext2](content2);
+
+  const sortedContent1 = Object.fromEntries(_.sortBy(Object.entries(parsedContent1)));
+  const sortedContent2 = Object.fromEntries(_.sortBy(Object.entries(parsedContent2)));
+
+  for (const key of Object.keys(sortedContent1)) {
+    const value1 = sortedContent1[key];
+    const value2 = sortedContent2[key];
+
+    if (!value2) {
+      result = `${result}\n- ${key}: ${value1}`;
+    }
+
+    if (value1 === value2) {
+      result = `${result}\n  ${key}: ${value1}`;
+    }
+
+    if (value1 !== value2 && value2) {
+      result = `${result}\n- ${key}: ${value1}\n+ ${key}: ${value2}`;
+    }
+  }
+
+  for (const key of Object.keys(sortedContent2)) {
+    const value1 = sortedContent1[key];
+    const value2 = sortedContent2[key];
+
+    if (!value1) {
+      result = `${result}\n+ ${key}: ${value2}`;
+    }
+  }
+
+  return `{${result}
+}`;
+}
+
+export { genDiff };
