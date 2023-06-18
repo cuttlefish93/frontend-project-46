008aca0d6737e3cd5032a87d7623ebea4a96ff39
Kamila Begimkulova <cuttlefish93@gmail.com>
2023-06-18 20:29:43 +0500

    add calculating diff for nested collections

---
+++ src/formatters.js
@@ -0,0 +1,63 @@
+function stylish(coll) {
+  const signRepeatPerStage = 4;
+  const sign = ' ';
+  const openBracket = '{';
+  const closeBracket = '}';
+  let openBracketCounter = 0;
+  let closeBracketCounter = 0;
+  let stage;
+  let repeatedSign;
+
+  const json = JSON.stringify(coll, null, 2);
+  const formattedJson = json.replaceAll('"', '').replaceAll(',', '');
+
+  const jsonArr = formattedJson.split('\n').map((item) => {
+    let innerStageSings = 0;
+
+    const itemArr = item.split('');
+
+    if (itemArr.length === 1) {
+      if (itemArr[0] === openBracket) openBracketCounter += 1;
+      if (itemArr[0] === closeBracket) closeBracketCounter += 1;
+      return item;
+    }
+
+    const filteredItemArr = itemArr.filter((i) => i !== ' ');
+
+    if (filteredItemArr.includes(':')) {
+      const index = filteredItemArr.indexOf(':');
+      filteredItemArr.splice(index + 1, 0, sign);
+    }
+
+    if (filteredItemArr.includes(openBracket)) openBracketCounter += 1;
+
+    if (filteredItemArr.includes(closeBracket)) closeBracketCounter += 1;
+
+    if (filteredItemArr.includes('+') || filteredItemArr.includes('-')) {
+      innerStageSings = 2;
+      const index1 = filteredItemArr.indexOf('+');
+      const index2 = filteredItemArr.indexOf('-');
+      const index = index1 >= 0 ? index1 : index2;
+      filteredItemArr.splice(index + 1, 0, sign);
+    }
+
+    stage = openBracketCounter - closeBracketCounter;
+
+    if (filteredItemArr.includes(openBracket)) {
+      repeatedSign = sign.repeat((stage - 1) * signRepeatPerStage - innerStageSings);
+    } else {
+      repeatedSign = sign.repeat(stage * signRepeatPerStage - innerStageSings);
+    }
+
+    filteredItemArr.unshift(repeatedSign);
+    return filteredItemArr.join('');
+  });
+
+  return jsonArr.join('\n');
+}
+
+const formatters = {
+  stylish,
+};
+
+export default formatters;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 import * as path from 'node:path';
 import * as process from 'process';
 import * as fs from 'fs';
-import _ from 'lodash';
 import parsers from './parsers.js';
+import formatters from './formatters.js';
 
-function genDiff(filepath1, filepath2) {
-  let result = '';
+function sortCollection(coll) {
+  const newColl = {};
 
+  Object.keys(coll)
+    .sort((a, b) => {
+      const str1 = a.split(' ').at(-1);
+      const str2 = b.split(' ').at(-1);
+      if (str1 > str2) {
+        return 1;
+      }
+      if (str1 < str2) {
+        return -1;
+      }
+      return 0;
+    })
+    .forEach((key) => {
+      newColl[key] = coll[key];
+      if (typeof newColl[key] === 'object' && newColl[key] !== null) {
+        const sortedColl = sortCollection(newColl[key]);
+        newColl[key] = sortedColl;
+      }
+    });
+  return newColl;
+}
+
+function calcDiff(coll1, coll2) {
+  const diff = {};
+  const keys1 = Object.keys(coll1);
+
+  keys1.forEach((key) => {
+    const value1 = coll1[key];
+    const value2 = coll2[key];
+    const str1 = typeof value1 === 'object' ? value1 : String(value1);
+    const str2 = typeof value2 === 'object' ? value2 : String(value2);
+
+    // when one key is object and another key is not object
+    if ((typeof str1 === 'object' || typeof str2 === 'object') && typeof str1 !== typeof str2 && str1 !== 'undefined' && str2 !== 'undefined') {
+      diff[`- ${key}`] = str1;
+      diff[`+ ${key}`] = str2;
+      return;
+    }
+
+    // when key in coll1 exist and key in coll2 do not exist
+    if (str1 !== str2 && str2 === 'undefined') {
+      diff[`- ${key}`] = str1;
+      return;
+    }
+
+    // when key exist in both colls and has the same value (not objects)
+    if (str1 === str2 && typeof str1 !== 'object' && typeof str2 !== 'object') {
+      diff[key] = str1;
+      return;
+    }
+
+    // when key exist in both colls and has different value (not object)
+    if (str1 !== str2 && str2 !== 'undefined' && str2 !== null && typeof str1 !== 'object' && typeof str2 !== 'object') {
+      diff[`- ${key}`] = str1;
+      diff[`+ ${key}`] = str2;
+      return;
+    }
+
+    // when key in both colls is object
+    if (typeof str1 === 'object' && typeof str2 === 'object') {
+      const innerDiff = calcDiff(str1, str2);
+      diff[key] = innerDiff;
+    }
+  });
+
+  const keys2 = Object.keys(coll2);
+  const uniqueKeys2 = keys2.filter((key) => !keys1.includes(key));
+
+  uniqueKeys2.forEach((key) => {
+    const value1 = coll1[key];
+    const value2 = coll2[key];
+    const str1 = typeof value1 === 'object' ? value1 : String(value1);
+    const str2 = typeof value2 === 'object' ? value2 : String(value2);
+
+    if (str1 === 'undefined') {
+      diff[`+ ${key}`] = str2;
+    }
+  });
+
+  return diff;
+}
+
+function genDiff(filepath1, filepath2, formatter = 'stylish') {
   const fullPath1 = path.resolve(process.cwd(), filepath1);
   const fullPath2 = path.resolve(process.cwd(), filepath2);
 
@@ -19,40 +102,10 @@ function genDiff(filepath1, filepath2) {
   const parsedContent1 = parsers[ext1](content1);
   const parsedContent2 = parsers[ext2](content2);
 
-  const sortedContent1 = Object.fromEntries(_.sortBy(Object.entries(parsedContent1)));
-  const sortedContent2 = Object.fromEntries(_.sortBy(Object.entries(parsedContent2)));
-
-  const keys1 = Object.keys(sortedContent1);
-  const keys2 = Object.keys(sortedContent2);
-
-  keys1.forEach((key) => {
-    const value1 = sortedContent1[key];
-    const value2 = sortedContent2[key];
-
-    if (!value2) {
-      result = `${result}\n- ${key}: ${value1}`;
-    }
-
-    if (value1 === value2) {
-      result = `${result}\n  ${key}: ${value1}`;
-    }
-
-    if (value1 !== value2 && value2) {
-      result = `${result}\n- ${key}: ${value1}\n+ ${key}: ${value2}`;
-    }
-  });
-
-  keys2.forEach((key) => {
-    const value1 = sortedContent1[key];
-    const value2 = sortedContent2[key];
-
-    if (!value1) {
-      result = `${result}\n+ ${key}: ${value2}`;
-    }
-  });
-
-  return `{${result}
-}`;
+  const diff = calcDiff(parsedContent1, parsedContent2);
+  const sortedDiff = sortCollection(diff);
+  const formattedDiff = formatters[formatter](sortedDiff);
+  return formattedDiff;
 }
 
 export default genDiff;
